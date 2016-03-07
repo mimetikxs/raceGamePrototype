627c5eeb2f844bf4e83b457d29cdb6f5518f524a
Nestor Rubio <nestor.kxs@gmail.com>
2016-03-07 03:46:12 +0000

    separate track and background

--- src/assets.cpp
+++ src/assets.cpp
@@ -3,7 +3,7 @@
 void Assets::loadAssets(){
     loadImage(&collisionMap, "images/map.png");
     loadImage(&progressMap, "images/progess-map.png");
-    loadImage(&backgroundImg, "images/background.png");
+    loadImage(&backgroundImg, "images/background-track-only.png");
     
     loadImage(&bike1, "images/bikes/bike1.png");
     loadImage(&bike2, "images/bikes/bike2.png");
